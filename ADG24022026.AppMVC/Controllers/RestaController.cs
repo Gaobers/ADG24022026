71341783b2fe1dca20b04d90b1d10ad0671199ab
MINEDUCYT <MINEDUCYT@DESKTOP-CC30LNJ>
2026-02-24 11:26:36 -0600

    PRAC-3 Creacion de controlador funcional

---
+++ ADG24022026.AppMVC/Controllers/RestaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ADG24022026.AppMVC.Controllers
+{
+    public class RestaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(double numero1, double numero2)
+        {
+            double resultado = numero1 - numero2;
+            ViewBag.Resultado = resultado;
+            return View();
+        }
+
+
+    }
+
+}
