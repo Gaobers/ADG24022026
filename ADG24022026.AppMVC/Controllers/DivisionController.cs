a880c4c95ab449635e7978a3e8f6439c85292b91
Gabriel <gabrielobandogarcia100@gmail.com>
2026-02-24 08:56:35 -0600

    Creación de Controladores y Vistas

---
+++ ADG24022026.AppMVC/Controllers/DivisionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ADG24022026.AppMVC.Controllers
+{
+    public class DivisionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
