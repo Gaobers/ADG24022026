0c9aeefa4a681f57b9e48da68f7c95bd7ce04bd5
Gabriel <gabrielobandogarcia100@gmail.com>
2026-02-24 08:43:45 -0600

    Agregar archivos de proyecto.

---
+++ ADG24022026.AppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ADG24022026.AppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
