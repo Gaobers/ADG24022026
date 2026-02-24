a3ed5caa9cf76785db4eff8c8370b62ef358dbcc
Andrea <denissedepaz16@gmail.com>
2026-02-24 11:18:13 -0500

    Creacion de la vista y controlador

---
+++ ADG24022026.AppMVC/Controllers/SumaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADG24022026.AppMVC.Controllers
+{
+
+        public class SumaController : Controller
+        {
+            
+            public IActionResult Index()
+            {
+                return View();
+            }
+
+   
+            
+        }
+    }
