dfa72cd7e0e5bcec48cfbcbb3c3d4e7ea0b2047e
Andrea <denissedepaz16@gmail.com>
2026-02-24 11:53:40 -0500

    Archivos del proyecto Suma

--- ADG24022026.AppMVC/Controllers/SumaController.cs
+++ ADG24022026.AppMVC/Controllers/SumaController.cs
@@ -3,16 +3,29 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ADG24022026.AppMVC.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
 
+    namespace ADG24022026.Controllers
+    {
         public class SumaController : Controller
         {
-            
+            // Muestra el formulario al cargar la página
             public IActionResult Index()
             {
                 return View();
             }
 
-   
-            
+            // Recibe los números y realiza la suma
+            [HttpPost]
+            public IActionResult Index(double num1, double num2)
+            {
+                double resultado = num1 + num2;
+                ViewBag.Resultado = resultado; // Enviamos el dato a la vista
+                return View();
+            }
         }
     }
+
+
+}
+    
